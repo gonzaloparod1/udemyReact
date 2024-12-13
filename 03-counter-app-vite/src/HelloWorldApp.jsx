b828017f870b98a7fa7809883d618813f84d3911
gonzaloparod1 <gonzalo.parodi@gmail.com>
2024-12-13 19:45:31 -0300

    create functional components react vite

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,14 @@
+/*
+export function App(){
+    / document.createElement...
+    return (<h1>Hola Mundo!!!</h1>);
+}
+*/
+
+// export const HelloWorldApp = () => {
+//   return (
+//     <div>Hello World App</div>
+//   )
+// }
+
+export const HelloWorldApp = () => <h1>Arrow Function</h1>
---
+++ 03-counter-app-vite/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HelloWorldApp } from './HelloWorldApp';
+
+// import {App} from './HelloWorldApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <HelloWorldApp/>
+    </React.StrictMode>
+)
