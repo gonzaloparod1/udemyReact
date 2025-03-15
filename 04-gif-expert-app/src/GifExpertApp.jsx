fecb97abb8364e54fe24cea557d8ad7a70f13c83
Gonzalo <gonzalo.parodi@gmail.com>
2025-03-15 17:17:08 -0300

    fix package.json

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,11 @@
 export const GifExpertApp = () => {
     return (
         <>
+        {/* Titulo*/}
         <h1>GifExpert App</h1>
+        {/* SearchBar(Input) */}
+        {/* GifGrid(Listado de Gif) */}
+        {/* Gif Items */}
         </>
     )
 }
